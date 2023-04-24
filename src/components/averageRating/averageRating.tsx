b8ffdacfc8136b3051b6e6bbfdb96786cb4a17dc
andja <andja.velimirovic@gmail.com>
2023-04-24 13:23:15 +0200

    add average rating to cart item

---
+++ src/components/averageRating/averageRating.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import styles from './averageRating.module.scss';
+
+interface Props {
+	id: number;
+}
+
+export default function AverageRating({ id }: Props) {
+	const [rating, setRating] = useState<number>(0);
+	const [numberOfReviews, setNumberOfReviews] = useState<number>(0);
+
+	useEffect(() => {
+		const getComments = async (id: number) => {
+			try {
+				const res = await fetch(
+					`http://localhost:3000/api/products/${id}/comments`
+				);
+				if (res.ok) {
+					const data = await res.json();
+					setNumberOfReviews(data.length);
+					const averageRating =
+						data.reduce((acc: number, curr: Comments) => acc + curr.rating, 0) /
+						data.length;
+
+					setRating(averageRating);
+				}
+			} catch (err) {
+				console.log(err);
+			}
+		};
+
+		getComments(id);
+	}, [id]);
+
+	console.log(rating);
+
+	return (
+		<div className={styles.rating}>
+			{rating > 0 &&
+				[...Array(5)].map((_, index) => {
+					index += 1;
+					return (
+						<span
+							key={index}
+							className={`${index <= rating ? styles.starOn : styles.starOff}`}
+						>
+							<span className={styles.star}>&#9733;</span>
+						</span>
+					);
+				})}
+			<span className={styles.numOfReviews}> ({numberOfReviews})</span>
+		</div>
+	);
+}
--- src/pages/Cart/cartItem.tsx
+++ src/pages/Cart/cartItem.tsx
@@ -3,6 +3,7 @@ import { useCartContext } from '../../context/CartContext';
 import QuantityHandler from '../../components/quantityHandler/quantityHandler';
 import Button from '../../components/button/button';
 import styles from './cartItem.module.scss';
+import AverageRating from '../../components/averageRating/averageRating';
 
 interface Props {
 	product: Product;
@@ -27,7 +28,9 @@ function CartItem({ product: { id, img, title, info, price } }: Props) {
 				<img src={require(`../../assets${img}`)} alt={title} />
 			</div>
 			<div className={styles.cartItemContent}>
-				<h4>{title}</h4>
+				<h4 className={styles.cartItemTitle}>{title}</h4>
+				<AverageRating id={id} />
+
 				<span className={styles.cartItemPrice}>${price}</span>
 				<p className={styles.cartItemInfo}>{info}</p>
 				<div className={styles.cartItemQuantity}>
