29b6670f47a464569404894d3c466b4f58aeef6e
andja <andja.velimirovic@gmail.com>
2023-05-04 10:52:38 +0200

    separate interfaces and types files

---
+++ src/types/interfaces.d.ts
@@ -0,0 +1,16 @@
+interface IProduct {
+	id: number;
+	title: string;
+	info: string;
+	description: string;
+	price: string;
+	img: string;
+}
+
+interface IComments {
+	id: number;
+	title: string;
+	message: string;
+	user: string;
+	rating: number;
+}
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,18 +1,1 @@
-interface IProduct {
-	id: number;
-	title: string;
-	info: string;
-	description: string;
-	price: string;
-	img: string;
-}
-
-interface IComments {
-	id: number;
-	title: string;
-	message: string;
-	user: string;
-	rating: number;
-}
-
 type CreateCommentType = Omit<IComments, 'id'>;
